1b03fdb8fb08878086f7ebc779640f97cae5c38b
wondoil1234 <wondoil1234@gmail.com>
2026-05-18 18:40:05 +0900

    [로비 UI] 오류 수정

--- Assets/Scripts/DN_UI/RobbyUI.cs
+++ Assets/Scripts/DN_UI/RobbyUI.cs
@@ -13,7 +13,7 @@ public class RobbyUI : DaniTechUIBase
 
     public void OnClick_GameStart()
     {
-        
+        Debug.Log("게임을 시작합니다.");
 
 
         DaniTechUIManager.Instance.CloseContentUI(DaniTechUIType.RobbyUI);
@@ -21,6 +21,10 @@ public class RobbyUI : DaniTechUIBase
 
     public void OnClick_GameQuit()
     {
+        Debug.Log("게임을 종료합니다.");
+
+
+
         DaniTechGameManager.Inst.SaveAndEndGame();
     }
 
