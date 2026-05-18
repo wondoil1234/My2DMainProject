cb274280a35ad174e6dff2f61337fbd6570c8bca
wondoil1234 <wondoil1234@gmail.com>
2026-05-18 13:58:44 +0900

    [로비 UI] 코드 구현

--- Assets/Scripts/DN_UI/RobbyUI.cs
+++ Assets/Scripts/DN_UI/RobbyUI.cs
@@ -3,22 +3,25 @@
 public class RobbyUI : DaniTechUIBase
 {
     [SerializeField] private DaniTechUIButton Button_GameStart;
-    [SerializeField] private DaniTechUIButton Button_GaemQuit;
+    [SerializeField] private DaniTechUIButton Button_GameQuit;
 
     private void OnEnable()
     {
         Button_GameStart.BindOnClickButtonEvent(OnClick_GameStart);
-        Button_GaemQuit.BindOnClickButtonEvent(OnClick_GaemQuit);
+        Button_GameQuit.BindOnClickButtonEvent(OnClick_GameQuit);
     }
 
     public void OnClick_GameStart()
     {
+        
 
+
+        DaniTechUIManager.Instance.CloseContentUI(DaniTechUIType.RobbyUI);
     }
 
-    public void OnClick_GaemQuit()
+    public void OnClick_GameQuit()
     {
-
+        DaniTechGameManager.Inst.SaveAndEndGame();
     }
 
 
