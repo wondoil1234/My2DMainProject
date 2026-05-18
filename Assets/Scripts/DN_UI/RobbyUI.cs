9df54e2dd5385904b808695f68a8312586d8e74e
wondoil1234 <wondoil1234@gmail.com>
2026-05-18 13:36:05 +0900

    [로비 UI 프리팹]

---
+++ Assets/Scripts/DN_UI/RobbyUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class RobbyUI : DaniTechUIBase
+{
+    [SerializeField] private DaniTechUIButton Button_GameStart;
+    [SerializeField] private DaniTechUIButton Button_GaemQuit;
+
+    private void OnEnable()
+    {
+        Button_GameStart.BindOnClickButtonEvent(OnClick_GameStart);
+        Button_GaemQuit.BindOnClickButtonEvent(OnClick_GaemQuit);
+    }
+
+    public void OnClick_GameStart()
+    {
+
+    }
+
+    public void OnClick_GaemQuit()
+    {
+
+    }
+
+
+
+}
