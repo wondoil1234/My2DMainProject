199570c4cf1cf8ba19a54777c860ac4324141318
wondoil1234 <wondoil1234@gmail.com>
2026-05-19 16:37:47 +0900

    [도감 UI] 도감과 도감슬롯 코드 작성

---
+++ Assets/Scripts/DN_UI/GameBookSlotUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameBookSlotUI : MonoBehaviour
+{
+    [Header("슬롯 기본 정보")]
+    [SerializeField] private Image Image_MainIcon;
+    [SerializeField] private Text Text_MainName;
+    [SerializeField] private GameObject Gobj_Selected;
+}
---
+++ Assets/Scripts/DN_UI/GameBookUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameBookUI : DaniTechUIBase
+{
+    [Header("동적 생성할 프리팹")]
+    [SerializeField] private GameObject Prefab_Slot;
+
+    [Header("디테일 정보 영역")]
+    [SerializeField] private Image Image_MainIcon;
+    [SerializeField] private Text Text_MainName;
+    [SerializeField] private Text Text_Descripction;
+
+    //[Header("부가 정보")]
+    //[SerializeField] private GameObject Layout_SubInfoSkill;
+
+
+    [Header("슬롯 리스트 영역")]
+    [SerializeField] private Transform Transform_SlotRoot;
+
+
+}
