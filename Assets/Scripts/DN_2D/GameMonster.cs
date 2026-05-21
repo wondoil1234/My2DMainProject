e0310e996639d9b09d990e3dc18b07fc3cfdb233
wondoil1234 <wondoil1234@gmail.com>
2026-05-21 16:55:51 +0900

    [몬스터 UI] 데이터 드리븐 작성

---
+++ Assets/Scripts/DN_2D/GameMonster.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GameMonster : MonsterBase
+{
+
+    private int _instanceId;
+    private string _dataId;
+ 
+    public void InitMonster(int instanceId, string dataId)
+    {
+        _instanceId = instanceId;
+        _dataId = dataId;
+    }
+}
--- Assets/Scripts/DN_2D/SkillBase.cs
+++ Assets/Scripts/DN_2D/SkillBase.cs
@@ -1,16 +1,6 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-public class SkillBase : MonoBehaviour
+public class SkillBase : SkillProjectile
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
