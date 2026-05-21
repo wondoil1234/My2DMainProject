af5678a5c8caeee985c8018c865176883d0e2b8e
wondoil1234 <wondoil1234@gmail.com>
2026-05-21 17:54:55 +0900

    [몬스터 UI] 몬스터 생성 정보 셋팅

--- Assets/Scripts/DN_2D/GameMonster.cs
+++ Assets/Scripts/DN_2D/GameMonster.cs
@@ -5,10 +5,33 @@ public class GameMonster : MonsterBase
     [Header("데이터를 확인할 수 있도록 임시로 열어줌")]
     public int _instanceId;
     public string _dataId;
- 
+
+    [Header("받아왔는데 전투에서 필요한 데이터")]
+    private DNMonsterData _thisMonsterData;
+    public int _baseHp;
+    public int _baseAtk;
+
     public void InitMonster(int instanceId, string dataId)
     {
         _instanceId = instanceId;
         _dataId = dataId;
+
+        var monsterData = DaniTechGameDataManager.Instance.GetDNMonsterData(dataId);
+        if(monsterData != null )
+        {
+            _thisMonsterData = monsterData;
+            _baseHp = _thisMonsterData.BaseHp;
+            _baseAtk = _thisMonsterData.BaseAtk;
+        }
+    }
+
+    private int GetFinalNormalAtkDamage(int baseAtk, float normalAtkMultiple)
+    {
+        return GetFinalSkillDamage(baseAtk, normalAtkMultiple);
+    }
+
+    private int GetFinalSkillDamage(int baseAtk, float skillMultiple)
+    {
+        return (int)(baseAtk *  skillMultiple);
     }
 }
